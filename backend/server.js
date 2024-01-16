b34bd6e302622e55c8750d737cdf3d9d4b1b57e4
BrightITcodes <arobanisunday2019@nda.edu.ng>
2024-01-16 16:20:30 +0100

    fixing deployment error

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/userRoutes");
 const productRoute = require("./routes/productRoutes");
 const contactRoute = require("./routes/contactRoutes");
-const errorHandler = require("./middleWare/errorMiddleware");
+const errorHandler = require("./middleWare/errorMiddleWare");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
@@ -21,7 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://inventory-management-software-neon.vercel.app/",
+    ],
     credentials: true,
   })
 );
